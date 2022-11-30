d328a844420c9c9a350776b5818f15e6da6682cb
Egor Sablev <79162412+Stee11izard@users.noreply.github.com>
2022-12-01 02:57:58 +0300

    Delete text.py

--- text.py
+++
@@ -1,8 +0,0 @@
-import requests
-data={
-    "grant_type":"client_credentials",
-    "client_id":"courier",
-    "client_secret":"eWL%FhU6pj"
-}
-response = requests.post("http://auth.uat.svz.maxus.lan:443/access_token", data)
-print(response)
